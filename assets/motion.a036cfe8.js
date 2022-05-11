af1f233653b033ad4537b544fc024c3a0cd013f0
xiaoxian521 <1923740402@qq.com>
2022-05-11 07:31:10 +0000

    ci: Automatic deployment from github actions

---
+++ assets/motion.a036cfe8.js
@@ -0,0 +1,1 @@
+import{y as i,ar as o,a1 as a,ak as n}from"./index.4a448694.js";var s=i({name:"Motion",props:{delay:{type:Number,default:50}},render(){const{delay:t}=this,e=o("motion");return a(n("div",{},{default:()=>[this.$slots.default()]}),[[e,{initial:{opacity:0,y:100},enter:{opacity:1,y:0,transition:{delay:t}}}]])}});export{s as M};
